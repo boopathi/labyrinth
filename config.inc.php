aacd1623c6d91408668719b6e34b43fddff6cff5
Boopathi Rajaa <boopathi@live.com>
2011-11-02 15:38:39 +0530

    Initial COmmit

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Project	: Labyrinth
+ * Author 	: Boopathi Rajaa
+ * Concept	: Matrix
+ * Description:
+ * Configuration File - stores database login details and admin authentication
+ */
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","root");
+define("DB_NAME","labyrinth");

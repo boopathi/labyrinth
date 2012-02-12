8f7e930c176fac8d2d90f523813bb1051ce536e0
nobelium <vignesh_goldfish@yahoo.com>
2012-02-12 18:21:00 +0500

    admin auth4

--- common.lib.php
+++ common.lib.php
@@ -231,7 +231,7 @@ function isAdmin(){
 	 $isAdminQuery = mysql_query("SELECT `value` FROM `config` WHERE `key`='admin'") or die(mysql_error());
 	 $isAdmin = mysql_fetch_assoc($isAdminQuery) or die(mysql_error());
 	 $admins = explode(";",$isAdmin['admin']);
-	 
+	 print_r($admins);
 	 for($i=0;$i<count($admins);$i++){
 	 	     if($userid === intval($admins[$i])){
 				return true;

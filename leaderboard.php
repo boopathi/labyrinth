8ac894a73219c5e83c8c65066dfb0fdc903a100a
Boopathi Rajaa <boopathi@live.com>
2012-02-16 19:21:27 -0600

    update query and cached values to seperate table

--- leaderboard.php
+++ leaderboard.php
@@ -13,7 +13,17 @@ define("LABYRINTH_CONST", "LABYRINTH APPLICATION");
 include("./config.inc.php");
 include("./common.lib.php");
 
-connectDB();		
+connectDB();
+
+function insertIntoLeaderBoard(){
+	$truncate = mysql_query("truncate pragyan12_laby.leaderboard");
+	if(!$truncate)
+		return false;
+	$query = mysql_query("insert into pragyan12_laby.leaderboard (rownum, userid, level, attempts, user_email, user_fullname) select t3.rownum, t3.userid, t3.level, t3.attempts, t3.user_email, t3.user_fullname from (select @row:=@row+1 rownum, t2.userid, t2.level, t2.attempts, t2.user_email, t2.user_fullname from (select t1.userid, t1.attempts, t1.user_email, t1.user_fullname, t1.level from (select lu.userid, ua.attempts, cu.user_email, cu.user_fullname, lu.to level FROM pragyan12_laby.user_level lu, ( select att.userid, sum(att.attempts) attempts from pragyan12_laby.user_attempts att group by att.userid) ua, pragyan12_cms.pragyanV3_users cu WHERE lu.userid=cu.user_id AND lu.userid=ua.userid ORDER BY lu.to desc) t1 group by t1.userid order by t1.level desc, t1.attempts asc, t1.userid asc) t2, (select @row:=0) r) t3") or die(mysql_error());
+	
+	if($query) return true;
+	return false;
+}
 
 function updateLeaderBoard($page){
 	$page = escape($page);
@@ -22,7 +32,8 @@ function updateLeaderBoard($page){
 	$pagenum = intval($page);
 	$from = ($pagenum<1?0:$pagenum-1) * 30;
 	$to = 30;
-	$query = mysql_query("select t3.rownum, t3.userid, t3.level, t3.attempts, t3.user_email, t3.user_fullname from (select @row:=@row+1 rownum, t2.userid, t2.level, t2.attempts, t2.user_email, t2.user_fullname from (select t1.userid, t1.attempts, t1.user_email, t1.user_fullname, t1.level from (select lu.userid, ua.attempts, cu.user_email, cu.user_fullname, lu.to level FROM pragyan12_laby.user_level lu, ( select att.userid, sum(att.attempts) attempts from pragyan12_laby.user_attempts att group by att.userid) ua, pragyan12_cms.pragyanV3_users cu WHERE lu.userid=cu.user_id AND lu.userid=ua.userid ORDER BY lu.to desc) t1 group by t1.userid order by t1.level desc, t1.attempts asc, t1.userid asc) t2, (select @row:=0) r) t3 limit $from, $to") or die(mysql_error());
+	$query = mysql_query("select * from leaderboard limit $from, $to") or die(mysql_error());
+	//$query = mysql_query("select t3.rownum, t3.userid, t3.level, t3.attempts, t3.user_email, t3.user_fullname from (select @row:=@row+1 rownum, t2.userid, t2.level, t2.attempts, t2.user_email, t2.user_fullname from (select t1.userid, t1.attempts, t1.user_email, t1.user_fullname, t1.level from (select lu.userid, ua.attempts, cu.user_email, cu.user_fullname, lu.to level FROM pragyan12_laby.user_level lu, ( select att.userid, sum(att.attempts) attempts from pragyan12_laby.user_attempts att group by att.userid) ua, pragyan12_cms.pragyanV3_users cu WHERE lu.userid=cu.user_id AND lu.userid=ua.userid ORDER BY lu.to desc) t1 group by t1.userid order by t1.level desc, t1.attempts asc, t1.userid asc) t2, (select @row:=0) r) t3 limit $from, $to") or die(mysql_error());
 	$ret = array();
 	while($qresult = mysql_fetch_assoc($query))
 		$ret[] = array(
@@ -33,8 +44,12 @@ function updateLeaderBoard($page){
 	return $ret;
 }
 
-$numpages=14;
+$numpagesArr=mysql_fetch_array(mysql_query("select count(*) from leaderboard"));
+$numpages = intval($numpagesArr[0])/30;
 $pagenum = isset($_GET['page'])?$_GET['page']:1;
+
+if(isset($_GET['update']))
+	echo "Update Status: ". insertIntoLeaderBoard();
 
 ?>
 <html>
@@ -44,10 +59,12 @@ $pagenum = isset($_GET['page'])?$_GET['page']:1;
 		table {
 			width: 600px;
 			margin: 0 auto;
+			border: 1px solid #aaa;
 		}
 		table td {
 			min-width: 100px;
 		}
+		th { background: #AAA; color: black; }
 		tr.evenrow{
 			background: #222;
 		}
@@ -97,11 +114,11 @@ $pagenum = isset($_GET['page'])?$_GET['page']:1;
 		if($i%2)$class="evenrow";
 		else $class="oddrow";
 		$html = <<<ROW
-<tr class="$class">
-<td>{$statsarr[$i]["position"]}</td>
-<td>{$statsarr[$i]["level"]}</td>
-<td>{$statsarr[$i]["name"]}</td>
-</tr>
+		<tr class="$class">
+			<td>{$statsarr[$i]["position"]}</td>
+			<td>{$statsarr[$i]["level"]}</td>
+			<td>{$statsarr[$i]["name"]}</td>
+		</tr>
 ROW;
 		echo $html;
 

bfd52e1659aab61af80377a416abce68bca8a28e
Boopathi Rajaa <boopathi@live.com>
2012-02-16 18:16:42 -0600

    added leaderboard

---
+++ leaderboard.php
@@ -0,0 +1,122 @@
+<?php
+
+/* Project	: Labyrinth
+ * Author 	: Boopathi Rajaa
+ * Description:
+ */
+
+
+
+//Set the Constant LABYRINTH
+define("LABYRINTH_CONST", "LABYRINTH APPLICATION");
+
+include("./config.inc.php");
+include("./common.lib.php");
+
+connectDB();		
+
+function updateLeaderBoard($page){
+	$page = escape($page);
+	if(preg_match("/^[0-9]+$/", $page) == 0)
+		return "Cannot";
+	$pagenum = intval($page);
+	$from = ($pagenum<1?0:$pagenum-1) * 30;
+	$to = 30;
+	$query = mysql_query("select t3.rownum, t3.userid, t3.level, t3.attempts, t3.user_email, t3.user_fullname from (select @row:=@row+1 rownum, t2.userid, t2.level, t2.attempts, t2.user_email, t2.user_fullname from (select t1.userid, t1.attempts, t1.user_email, t1.user_fullname, t1.level from (select lu.userid, ua.attempts, cu.user_email, cu.user_fullname, lu.to level FROM pragyan12_laby.user_level lu, ( select att.userid, sum(att.attempts) attempts from pragyan12_laby.user_attempts att group by att.userid) ua, pragyan12_cms.pragyanV3_users cu WHERE lu.userid=cu.user_id AND lu.userid=ua.userid ORDER BY lu.to desc) t1 group by t1.userid order by t1.level desc, t1.attempts asc, t1.userid asc) t2, (select @row:=0) r) t3 limit $from, $to") or die(mysql_error());
+	$ret = array();
+	while($qresult = mysql_fetch_assoc($query))
+		$ret[] = array(
+			"position" => $qresult["rownum"],
+			"level"=>$qresult["level"],
+			"name" =>$qresult["user_fullname"]
+		);
+	return $ret;
+}
+
+$numpages=14;
+$pagenum = isset($_GET['page'])?$_GET['page']:1;
+
+?>
+<html>
+	<head>
+		<title>Labyrinth - LeaderBoard</title>
+		<style type="text/css">
+		table {
+			width: 600px;
+			margin: 0 auto;
+		}
+		table td {
+			min-width: 100px;
+		}
+		tr.evenrow{
+			background: #222;
+		}
+		tr.oddrow {
+			background: #111;
+		}
+		div.pageination{
+			background: #030303;
+			padding: 3px;
+			margin: 20px 0;
+		}
+		div.pageination a {
+			color: #999;
+			padding: 3px;
+			margin: 0 5px;
+			background: #111;
+			text-decoration: none;
+		}
+		div.pageination a.currentpage {
+			font-weight: bold;
+			color: #ff3333;
+			background: #222;
+		}
+		</style>
+	</head>
+	<body style="text-align:center;background: black;color: white; font-family: sans-serif;"> 
+	<h1>Labyrinth - LeaderBoard</h1>
+	<div class="pageination">
+	<?php
+        for($i=1;$i<=$numpages;$i++) {
+		if($pagenum == $i) $current="currentpage";
+		else $current="";
+                echo "<a class='$current' href='./leaderboard.php?page=$i'>$i</a> ";
+        }
+        ?>
+	</div>
+	<table style="text-align:center" cellspacing="0" cellpadding="5">
+		<tbody>
+		<tr>
+		<th>Position</th>
+		<th>Level</th>
+		<th>Name</th>
+		</tr>
+<?php
+	$statsarr = updateLeaderBoard($pagenum);
+	for($i=0;$i<count($statsarr);$i++){
+		if($i%2)$class="evenrow";
+		else $class="oddrow";
+		$html = <<<ROW
+<tr class="$class">
+<td>{$statsarr[$i]["position"]}</td>
+<td>{$statsarr[$i]["level"]}</td>
+<td>{$statsarr[$i]["name"]}</td>
+</tr>
+ROW;
+		echo $html;
+
+	}
+?>
+
+	</tbody>
+	</table>
+	<div class="pageination">
+	<?php
+	for($i=1;$i<=$numpages;$i++) {
+		echo "<a href='./leaderboard.php?page=$i'>$i</a> ";
+	}
+	?>
+	</div>
+	</body>
+</html>
+<?php
